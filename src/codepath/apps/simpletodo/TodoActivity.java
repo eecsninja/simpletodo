fbb09b7ecc9e06d169b26b78b78711680f3e36de
Simon Que <simonque@gmail.com>
2014-05-18 09:48:07 -0700

    Add support for adding items

--- src/codepath/apps/simpletodo/TodoActivity.java
+++ src/codepath/apps/simpletodo/TodoActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.os.Build;
 
@@ -54,5 +55,11 @@ public class TodoActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	public void addTodoItem(View v) {
+		EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+		itemsAdapter.add(etNewItem.getText().toString());
+		etNewItem.setText("");
 	}
 }
